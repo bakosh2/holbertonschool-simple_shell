0c1d10098d2e27c4d91fee52e0c6e013b2303f50
“bakosh2” <bakosh2@hotmail.com>
2025-12-27 22:56:53 +0000

    Fix non-interactive mode command execution

--- shell.c
+++ shell.c
@@ -9,6 +9,8 @@ extern char **environ;
 
 /**
  * shell_loop - Runs the main loop of the simple shell
+ *
+ * Reads input from the user, executes commands, and waits for child processes.
  */
 void shell_loop(void)
 {
@@ -19,25 +21,42 @@ void shell_loop(void)
 
 	while (getline(&line, &len, stdin) != -1)
 	{
+		/* Skip empty lines */
 		if (line[0] == '\n')
 			continue;
 
+		/* Remove trailing newline */
 		line[strcspn(line, "\n")] = '\0';
 
+		/* Fork a child process to execute the command */
 		pid = fork();
 		if (pid == 0)
 		{
-			char *argv[] = {line, NULL};
+			/* Prepare argv array */
+			char *argv[2];
+			argv[0] = line;
+			argv[1] = NULL;
 
+			/* Execute command */
 			execve(line, argv, environ);
+
+			/* If execve fails */
 			perror("Error");
 			exit(EXIT_FAILURE);
 		}
-		else
+		else if (pid > 0)
 		{
+			/* Parent waits for child to finish */
 			wait(&status);
 		}
+		else
+		{
+			/* Fork failed */
+			perror("Fork failed");
+			break;
+		}
 	}
+
 	free(line);
 }
 
