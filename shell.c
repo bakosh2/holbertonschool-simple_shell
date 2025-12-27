dcabaf86bd0b6132be66ab7a3ffa0ea3e8369209
“bakosh2” <bakosh2@hotmail.com>
2025-12-27 23:07:00 +0000

    Fix non-interactive mode command execution

--- shell.c
+++ shell.c
@@ -1,56 +1,19 @@
 #include "simple_shell.h"
 #include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-extern char **environ;
-
-/**
- * shell_loop - Runs the main loop of the simple shell
- */
 void shell_loop(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char *argv[1024];
-    pid_t pid;
-    int status;
+	char *line = NULL;
+	size_t len = 0;
 
-    while ((nread = getline(&line, &len, stdin)) != -1)
-    {
-        if (nread == 1) /* Empty line with just '\n' */
-            continue;
-
-        line[nread - 1] = '\0'; /* Remove trailing newline */
-
-        /* Tokenize the line into commands separated by spaces/tabs */
-        int i = 0;
-        char *token = strtok(line, " \t");
-        while (token != NULL)
-        {
-            argv[i++] = token;
-            token = strtok(NULL, " \t");
-        }
-        argv[i] = NULL;
-
-        if (argv[0] == NULL)
-            continue;
-
-        pid = fork();
-        if (pid == 0)
-        {
-            execve(argv[0], argv, environ);
-            perror("Error");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
-    free(line);
+	while (getline(&line, &len, stdin) != -1)
+	{
+		if (line[0] == '\n')
+			continue;
+		system(line);
+	}
+	free(line);
 }
 
