88c208208c1a575ca0ac96d563b4b6917919ecce
“bakosh2” <bakosh2@hotmail.com>
2025-12-27 22:51:00 +0000

    Fix non-interactive mode command execution

--- shell.c
+++ shell.c
@@ -1,11 +1,43 @@
 #include "simple_shell.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdio.h>
+
+extern char **environ;
 
 /**
- * shell_loop - main loop of the shell
- *
- * Return: void
+ * shell_loop - Runs the main loop of the simple shell
  */
 void shell_loop(void)
 {
+	char *line = NULL;
+	size_t len = 0;
+	pid_t pid;
+	int status;
+
+	while (getline(&line, &len, stdin) != -1)
+	{
+		if (line[0] == '\n')
+			continue;
+
+		line[strcspn(line, "\n")] = '\0';
+
+		pid = fork();
+		if (pid == 0)
+		{
+			char *argv[] = {line, NULL};
+
+			execve(line, argv, environ);
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+	free(line);
 }
 
