77849a3f3bd4edacc18997c00ea2a8405080c394
“bakosh2” <bakosh2@hotmail.com>
2025-12-27 23:13:25 +0000

    Fix non-interactive mode command execution

--- shell.c
+++ shell.c
@@ -1,19 +1,54 @@
 #include "simple_shell.h"
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char **environ;
 
 void shell_loop(void)
 {
-	char *line = NULL;
-	size_t len = 0;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
 
-	while (getline(&line, &len, stdin) != -1)
-	{
-		if (line[0] == '\n')
-			continue;
-		system(line);
-	}
-	free(line);
+    while ((nread = getline(&line, &len, stdin)) != -1)
+    {
+        if (nread == 1) /* Empty line */
+            continue;
+
+        line[nread - 1] = '\0'; /* Remove newline */
+
+        /* Split line into argv */
+        char *argv[1024];
+        int i = 0;
+        char *token = strtok(line, " \t");
+        while (token)
+        {
+            argv[i++] = token;
+            token = strtok(NULL, " \t");
+        }
+        argv[i] = NULL;
+
+        if (argv[0] == NULL)
+            continue;
+
+        /* Fork and execute */
+        pid = fork();
+        if (pid == 0)
+        {
+            execve(argv[0], argv, environ);
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(&status);
+        }
+    }
+    free(line);
 }
 
