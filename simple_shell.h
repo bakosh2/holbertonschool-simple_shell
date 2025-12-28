8c51e08e3526c4085ed1d274a920c20e8210ad7d
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-28 14:59:57 +0300

    decler prototyp in main.c file

--- simple_shell.h
+++ simple_shell.h
@@ -8,7 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int _exec_cmd(char *cmd, char *prog_name, char **env);
+int _exec_cmd(char *cmd, char *prog_name, char **env); /* shell.c */
+int main(int argc, char **argv, char **env); /* main.c */
 
 #endif /* SIMPLE_SHELL_H */
-
