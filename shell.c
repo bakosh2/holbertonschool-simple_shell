33eaa63321d2021131dcf1206605c0b11fb54afe
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-28 14:49:13 +0300

    declared prog_name to compaile

--- shell.c
+++ shell.c
@@ -2,37 +2,39 @@
 
 
 /**
- * _exec_cmd - execute a command using execve
- * @cmd: command to execute (full path)
- * @prog_name: name of the shell (argv[0])
- * @env: environment variables
- * Return: 0 on success, -1 on failure
- */
+* _exec_cmd - execute a command using execve
+* @cmd: command to execute (full path)
+* @prog_name: name of the shell (argv[0])
+* @env: environment variables
+* Return: 0 on success, -1 on failure
+*/
 int _exec_cmd(char *cmd, char *prog_name, char **env)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    if (cmd == NULL || *cmd == '\0')
-        return (-1);
+	(void)prog_name; /* add for unused parameter for now */
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        return (-1);
-    }
+	if (cmd == NULL || *cmd == '\0')
+		return (-1);
 
-    if (pid == 0) /* child process */
-    {
-        execve(cmd, NULL, env);
-        /* If execve fails */
-        exit(127);
-    }
-    else /* parent process */
-    {
-        waitpid(pid, &status, 0);
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);
+	}
 
-    return (0);
+	if (pid == 0) /* child process */
+	{
+		execve(cmd, NULL, env);
+		/* If execve fails */
+		exit(127);
+	}
+	else /* parent process */
+	{
+		waitpid(pid, &status, 0);
+	}
+
+	return (0);
 }
