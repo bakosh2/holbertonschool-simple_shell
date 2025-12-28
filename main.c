e27089c4bb0898c4c9908bf1f3d27f8f8fae3458
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-28 16:04:25 +0300

    edit for /bin/ls

--- main.c
+++ main.c
@@ -2,43 +2,45 @@
 
 
 /**
- * main - Entry point for the simple shell
- * @argc: argument count (not used)
- * @argv: argument vector
- * @env: environment variables
- * Return: 0 on success, or exit with status 1 on failure
- */
+* main - Entry point for the simple shell
+* @argc: argument count (not used)
+* @argv: argument vector
+* @env: environment variables
+* Return: 0 on success, or exit with status 1 on failure
+*/
 int main(int argc, char **argv, char **env)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	char *line = NULL;
 
-    (void)argc;
+	size_t len = 0;
+	ssize_t nread;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
+	(void)argc;
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1) /* Ctrl+D */
-        {
-            write(STDOUT_FILENO, "\n", 1);
-            break;
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-        /* remove newline at end */
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
+		nread = getline(&line, &len, stdin);
+		if (nread == -1) /* EOF */
+		{
+		if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+		break;
+	}
 
-        if (_exec_cmd(line, argv[0], env) == -1)
-        {
-            fprintf(stderr, "%s: 1: %s: not found\n", argv[0], line);
-        }
-    }
+		/* remove newline at end */
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
-    free(line);
-    return (0);
+		if (_exec_cmd(line, argv[0], env) == -1)
+		{
+			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], line);
+		}
+	}
+
+	free(line);
+	return (0);
 }
 
