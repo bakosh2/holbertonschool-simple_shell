318b9bac5faaec5dd53146ecf3c5fa5e6bcd1985
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-29 11:07:12 +0300

    fit /bin/ls copy

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include "simple_shell.h"
 
 /**
-* main - Entry point for the simple shell
-* @argc: argument count (not used)
-* @argv: argument vector
-* @env: environment variables
-* Return: 0 on success, or exit with status 1 on failure
-*/
+ * main - Entry point for the simple shell
+ * @argc: argument count (unused)
+ * @argv: argument vector
+ * @env: environment variables
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char **argv, char **env)
 {
 	char *line = NULL;
@@ -22,22 +23,20 @@ int main(int argc, char **argv, char **env)
 			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
 		nread = getline(&line, &len, stdin);
-		if (nread == -1) /* EOF */
+		if (nread == -1)
 		{
-		if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "\n", 1);
-		break;
-	}
-		/* remove newline at end */
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
 		if (line[nread - 1] == '\n')
 			line[nread - 1] = '\0';
 
-		/* skip leading spaces */
 		cmd = line;
 		while (*cmd == ' ' || *cmd == '\t')
 			cmd++;
 
-		/* if line is empty or spaces only, continue */
 		if (*cmd == '\0')
 			continue;
 
@@ -50,10 +49,8 @@ int main(int argc, char **argv, char **env)
 			*p = '\0';
 		}
 
-		if (_exec_cmd(cmd, argv[0], env) == -1)
-		{
+		if (execute_cmd(cmd, argv[0], env) == -1)
 			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], cmd);
-		}
 	}
 
 	free(line);
