4553893112b6ac2e52988dd788269591153f1d57
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-29 14:09:33 +0300

    fix /bin/ls copy

--- shell.c
+++ shell.c
@@ -1,49 +1,61 @@
 #include "simple_shell.h"
 
 /**
- * execute_cmd - Executes a command using execve
- * @cmd: command line
- * @prog_name: program name
+ * execute_cmd - execute a command using execve
+ * @cmd: command line entered by the user
+ * @prog_name: name of the shell
  * @env: environment variables
  *
  * Return: 0 on success, -1 on failure
  */
 int execute_cmd(char *cmd, char *prog_name, char **env)
 {
-        pid_t pid;
-        int status;
-        char *argv_exec[64];
-        char *token;
-        int i = 0;
+	pid_t pid;
+	int status;
+	char *argv_exec[3];
+	char *arg = NULL;
+	char *p;
 
-        (void)prog_name;
+	(void)prog_name;
 
-        if (cmd == NULL || *cmd == '\0')
-                return (-1);
+	if (cmd == NULL || *cmd == '\0')
+		return (-1);
 
-        /* tokenize command */
-        token = strtok(cmd, " ");
-        while (token && i < 63)
-        {
-                argv_exec[i++] = token;
-                token = strtok(NULL, " ");
-        }
-        argv_exec[i] = NULL;
+	/* Extract optional single argument (Task 2 special case) */
+	p = cmd;
+	while (*p && *p != ' ' && *p != '\t')
+		p++;
 
-        pid = fork();
-        if (pid == -1)
-                return (-1);
+	if (*p)
+	{
+		*p = '\0';
+		p++;
+		while (*p == ' ' || *p == '\t')
+			p++;
+		if (*p)
+			arg = p;
+	}
 
-        if (pid == 0)
-        {
-                execve(argv_exec[0], argv_exec, env);
-                exit(127);
-        }
-        else
-        {
-                waitpid(pid, &status, 0);
-        }
+	argv_exec[0] = cmd;
+	argv_exec[1] = arg;
+	argv_exec[2] = NULL;
 
-        return (0);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);
+	}
+
+	if (pid == 0)
+	{
+		execve(cmd, argv_exec, env);
+		exit(127);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+
+	return (0);
 }
-
