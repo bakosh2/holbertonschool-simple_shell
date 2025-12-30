a045c957ba033016e1e8891996aceede0ddfdf38
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-30 14:36:10 +0300

    edit code to use environ as a globel pinter

--- shell.c
+++ shell.c
@@ -1,19 +1,19 @@
 #include "simple_shell.h"
 
 /**
-* execute_cmd - execute a command with PATH handling
-* @cmd: command line
-* @prog_name: shell name
-*
-* Return: 0 always
-*/
+ * execute_cmd - execute a command with PATH handling
+ * @cmd: command line
+ * @prog_name: shell name
+ *
+ * Return: 0 always
+ */
 int execute_cmd(char *cmd, char *prog_name)
 {
-	char **argv = NULL;
-
-	char *path = NULL;
-
+	char **argv;
+	char *path;
 	pid_t pid;
+	int status;
+
 
 	argv = split_line(cmd);
 	if (!argv || !argv[0])
@@ -22,7 +22,7 @@ int execute_cmd(char *cmd, char *prog_name)
 		return (0);
 	}
 
-	if (strchr(argv[0], '/'))	/* absolute or relative path */
+	if (strchr(argv[0], '/'))
 		path = strdup(argv[0]);
 	else
 		path = find_in_path(argv[0]);
@@ -31,21 +31,21 @@ int execute_cmd(char *cmd, char *prog_name)
 	{
 		fprintf(stderr, "%s: 1: %s: not found\n", prog_name, argv[0]);
 		free_argv(argv);
-		return (0);	/* IMPORTANT: no fork */
+		return (0); /* NO fork */
 	}
 
 	pid = fork();
 	if (pid == 0)
 	{
-	if (pid == 0)
-	{
-		execv(path, argv);
+		execve(path, argv, environ);
 		exit(127);
+	}
+	else if (pid > 0)
+	{
+		waitpid(pid, &status, 0);
 	}
 
 	free(path);
 	free_argv(argv);
-	}
-
 	return (0);
 }
