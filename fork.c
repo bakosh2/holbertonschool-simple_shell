9f3ceeb35ef1827ffa0145afc59c7d2edba21879
badriahalmalki <badryahbalmalki@gmail.com>
2025-12-30 21:22:56 +0300

    edit to handel more path conditions

--- fork.c
+++ fork.c
@@ -1,69 +1,75 @@
 #include "simple_shell.h"
 
 /**
- * find_command - locate executable in PATH
+ * find_command - finds the full path of a command
  * @cmd: command name
  * @env: environment variables
  *
- * Return: malloc'd full path or NULL
+ * Return: full path if found, NULL otherwise
  */
 char *find_command(char *cmd, char **env)
 {
-	char *path_env = NULL, *path_copy, *dir, *full;
-	struct stat st;
-	int i, len;
+    char *path_env = NULL, *path_copy, *dir, *full;
+    struct stat st;
+    int i, len;
 
-	(void)env;
+    /* If command starts with / or ./ or ../ → use it directly */
+    if (cmd[0] == '/' || cmd[0] == '.')
+    {
+        if (stat(cmd, &st) == 0)
+            return (_strdup(cmd));
+        return (NULL);
+    }
 
-	if (cmd[0] == '/' || cmd[0] == '.')
-	{
-		if (stat(cmd, &st) == 0)
-			return (_strdup(cmd));
-		return (NULL);
-	}
+    /* Find PATH in environment */
+    for (i = 0; env && env[i]; i++)
+    {
+        if (_strncmp(env[i], "PATH=", 5) == 0)
+        {
+            path_env = env[i] + 5;
+            break;
+        }
+    }
 
-	for (i = 0; env && env[i]; i++)
-	{
-		if (_strncmp(env[i], "PATH=", 5) == 0)
-		{
-			path_env = env[i] + 5;
-			break;
-		}
-	}
+    /* No PATH variable at all */
+    if (!path_env)
+        return (NULL);
 
-	if (!path_env || *path_env == '\0')
-		return (NULL);
+    /* PATH="" → treat as no PATH */
+    if (*path_env == '\0')
+        return (NULL);
 
-	path_copy = _strdup(path_env);
-	if (!path_copy)
-		return (NULL);
+    /* Duplicate PATH so strtok doesn't modify env */
+    path_copy = _strdup(path_env);
+    if (!path_copy)
+        return (NULL);
 
-	dir = strtok(path_copy, ":");
-	while (dir)
-	{
-		len = _strlen(dir) + _strlen(cmd) + 2;
-		full = malloc(len);
-		if (!full)
-		{
-			free(path_copy);
-			return (NULL);
-		}
+    dir = strtok(path_copy, ":");
+    while (dir)
+    {
+        len = _strlen(dir) + _strlen(cmd) + 2;
+        full = malloc(len);
+        if (!full)
+        {
+            free(path_copy);
+            return (NULL);
+        }
 
-		full[0] = '\0';
+        full[0] = '\0';
         _strcpy(full, dir);
         _strcat(full, "/");
         _strcat(full, cmd);
 
-		if (stat(full, &st) == 0)
-		{
-			free(path_copy);
-			return (full);
-		}
+        if (stat(full, &st) == 0)
+        {
+            free(path_copy);
+            return (full);
+        }
 
-		free(full);
-		dir = strtok(NULL, ":");
-	}
+        free(full);
+        dir = strtok(NULL, ":");
+    }
 
-	free(path_copy);
-	return (NULL);
+    free(path_copy);
+    return (NULL);
 }
