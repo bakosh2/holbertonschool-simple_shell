9e045da73bd65dd4bdc28874d6777c83c0e0d7b8
“bakosh2” <bakosh2@hotmail.com>
2025-12-31 17:43:04 +0000

    Fix shell exit status and ignore generated binaries

--- main.c
+++ main.c
@@ -6,44 +6,45 @@
  * @argv: argument vector
  * @env: environment variables
  *
- * Return: Always 0
+ * Return: exit status of last command
  */
 int main(int argc, char **argv, char **env)
 {
-	char *line = NULL, *token;
-	char *tokens[64];
-	size_t len = 0;
-	ssize_t nread;
-	int i;
+        char *line = NULL, *token;
+        char *tokens[64];
+        size_t len = 0;
+        ssize_t nread;
+        int i;
+        int last_status = 0;
 
-	(void)argc;
+        (void)argc;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
+        while (1)
+        {
+                if (isatty(STDIN_FILENO))
+                        write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
-			break;
+                nread = getline(&line, &len, stdin);
+                if (nread == -1)
+                        break;
 
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+                if (line[nread - 1] == '\n')
+                        line[nread - 1] = '\0';
 
-		token = strtok(line, " \t");
-		i = 0;
-		while (token && i < 63)
-		{
-			tokens[i++] = token;
-			token = strtok(NULL, " \t");
-		}
-		tokens[i] = NULL;
+                token = strtok(line, " \t");
+                i = 0;
+                while (token && i < 63)
+                {
+                        tokens[i++] = token;
+                        token = strtok(NULL, " \t");
+                }
+                tokens[i] = NULL;
 
-		if (tokens[0])
-			execution(tokens, env, argv[0]);
-	}
+                if (tokens[0])
+                        last_status = execution(tokens, env, argv[0]);
+        }
 
-	free(line);
-	return (0);
+        free(line);
+        return (last_status);
 }
 
